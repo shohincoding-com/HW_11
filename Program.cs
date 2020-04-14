3bce055b9563d173daac800145ff0b8011ae79fd
shohincoding-com <salomov.shokhin@gmail.com>
2020-04-14 12:51:50 +0500

    Added new class for DB_MYSQlServer

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HW_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Alif
+    {
+        class Staff
+        {
+            public string Lastname;
+            public string Firstname;
+            public string Middlename;
+            public int BirthYear;
+            public string Position;
+            public int TheSalary;
+            public string Departament;
+        }
+
+        class StudentAcademy
+        {
+            public string Lastname;
+            public string Firstname;
+            public string Middlename;
+            public int BirthYear;
+            public string Position;
+        }
+
+    }
+}
